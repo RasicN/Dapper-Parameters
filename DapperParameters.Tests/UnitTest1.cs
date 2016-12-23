9f8705ddfc40006886dce861c5948cfc394a2dd0
joe.cisar <joe.cisar@VUHL-8PJKF72>
2016-12-23 16:26:23 -0600

    added table method

---
+++ DapperParameters.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomTestValues;
+
+namespace DapperParameters.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private DynamicParameters _parameters;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _parameters = new DynamicParameters();
+        }
+        
+        [TestMethod]
+        public void AddInt()
+        {
+            int intValue = RandomValue.Int();
+            _parameters.AddInt("test", intValue);
+
+            _parameters.ParameterNames.Should().Contain("test");
+            
+            var value = _parameters.Get<int>("test");
+            value.ShouldBeEquivalentTo(intValue);
+        }
+
+        [TestMethod]
+        public void AddString()
+        {
+            var stringValue = RandomValue.String();
+            var name = RandomValue.String();
+            _parameters.AddString(name, stringValue);
+
+            _parameters.ParameterNames.Should().Contain(name);
+
+            var value = _parameters.Get<string>(name);
+            value.ShouldBeEquivalentTo(stringValue);
+        }
+
+        [TestMethod]
+        public void Table()
+        {
+            // Act
+            _parameters.AddTable("test", RandomValue.List<IntListType>());
+
+            // Assert
+            var x = _parameters.Get<SqlMapper.ICustomQueryParameter>("test");
+            x.Should().NotBeNull();
+        }
+    }
+
+    public class IntListType
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DapperParameters/DynamicParametersExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Dapper;
+
+namespace DapperParameters
+{
+    public static class DynamicParametersExtensions
+    {
+        public static void AddInt(this DynamicParameters source, string name, int value)
+        {
+            source.Add(name, value, DbType.Int32);
+        }
+
+        public static void AddString(this DynamicParameters source, string name, string value)
+        {
+            source.Add(name, value, DbType.String);
+        }
+
+        public static void AddTable<T>(this DynamicParameters source, string name, ICollection<T> values)
+        {
+            var table = new DataTable();
+
+            var properties = typeof(T).GetRuntimeProperties().ToArray();
+
+            foreach (var prop in properties)
+            {
+                if (prop.SetMethod == null)
+                {
+                    // Property doesn't have a public setter so let's ignore it
+                    continue;
+                }
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            foreach (var value in values)
+            {
+                var parameters = new List<object>();
+                var valueProperties = value.GetType().GetRuntimeProperties().ToArray();
+                
+                foreach (var prop in valueProperties)
+                {
+                    var i = 0;
+                    if (prop.SetMethod == null)
+                    {
+                        // Property doesn't have a public setter so let's ignore it
+                        continue;
+                    }
+                    var p = prop.GetValue(value);
+                    parameters.Add(p);
+                }
+
+                table.Rows.Add(parameters.ToArray());
+            }
+
+            source.Add(name, table.AsTableValuedParameter(name));
+        }
+    }
+}
